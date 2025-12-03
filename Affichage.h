9e848a2557ec33940c9037c7decfdf33c40feca9
lois_b <loisbellavance@gmail.com>
2025-12-03 19:20:49 +0100

    Classe abstraite Affichage

--- Affichage.h
+++ Affichage.h
@@ -1,16 +1,19 @@
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
 
-class Grille;
+#include "Grid.h"
+
+class Grid;
 
 class Affichage{
     public:
         Affichage(){}
-        virtual void render(Grille* grille) = 0;
+        virtual void render(Grid* grille) = 0;
         virtual void clear() = 0;    
         virtual void init() = 0;
         virtual void close() = 0;
         virtual bool handleInput() = 0;
+        virtual bool estOuvert() = 0;
         virtual ~Affichage(){}
     };
 
